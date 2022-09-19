1bcee3faf87d347ea52dcb39034694d2048098d4
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2022-09-19 00:02:40 -0300

    BaSP-W04: ex05-for

---
+++ Semana-04/exercises/05_for.js
@@ -0,0 +1,56 @@
+console.log('--EXERCISE 5: FOR');
+
+/* a. Crear un array que contenga 5 palabras y recorrer dicho array
+utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.*/
+
+console.log('\n-Exercise 5.a:');
+
+var array = ['cat', 'cow', 'racoon', 'fox', 'chicken'];
+
+for (var i = 0; i < array.length; i++) {
+  alert(array[i]);
+}
+
+/* b. Al array anterior convertir la primera letra de cada
+palabra en mayúscula y mostrar una alerta por cada palabra modificada.*/
+
+console.log('\n-Exercise 5.b:');
+
+var upperCaseArray = [];
+var lowerCaseArray = [];
+
+for (var i = 0; i < array.length; i++) {
+  upperCaseArray = array[i].substring(0, 1).toUpperCase();
+  lowerCaseArray = array[i].substring(1, array[i].length).toLowerCase();
+
+  alert(upperCaseArray + lowerCaseArray);
+}
+
+/* c. Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a)
+recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence.
+Al final mostrar una única alerta con la cadena completa.*/
+
+console.log('\n-Exercise 5.c:');
+
+var sentence = '';
+
+for (var i = 0; i < array.length; i++) {
+  sentence += array[i];
+}
+
+alert(sentence);
+
+/* d. Crear una array vacío y con un bucle for de 10 repeticiones.
+Llenar el array con el número de la repetición,
+es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array,
+desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log). */
+
+console.log('\n-Exercise 5.d:');
+
+var myArray = [];
+
+for (var i = 0; i < 10; i++) {
+  myArray.push(i);
+}
+
+console.log(myArray)
