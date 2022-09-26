fa88ff6f10fea4960be2988a01210cd313ef4d84
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2022-09-26 01:44:22 -0300

    BaSP-W06-2022: changes made to invalid fields in login section

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -3,6 +3,8 @@ window.onload = function() {
   var email = document.getElementById('email');
   var password = document.getElementById('password');
   var button = document.getElementById('button');
+  var emailMessage = document.getElementById('message1');
+  var passwordMessage = document.getElementById('message2');
   var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
   var passwordExpression = /^([a-zA-Z0-9]+)$/
 
@@ -18,13 +20,16 @@ window.onload = function() {
     if (validateEmail() == false) {
       email.classList.remove('border', 'correct');
       email.classList.add('error');
+      emailMessage.style.visibility = 'visible'
     } else {
         email.classList.remove('border','error');
         email.classList.add('correct');
+        emailMessage.style.visibility = 'hidden'
     }
   }
 
   email.onfocus = function() {
+    emailMessage.style.visibility = 'hidden'
     if (validateEmail() == false) {
       email.value = '';
     }
@@ -38,13 +43,16 @@ window.onload = function() {
     if (validatePassword() == false) {
       password.classList.remove('border', 'correct');
       password.classList.add('error');
+      passwordMessage.style.visibility = 'visible'
     } else {
         password.classList.remove('border','error');
         password.classList.add('correct');
+        passwordMessage.style.visibility = 'hidden'
     }
   }
 
   password.onfocus = function() {
+    passwordMessage.style.visibility = 'hidden'
     if (validatePassword() == false) {
       password.value = '';
     }
@@ -53,7 +61,7 @@ window.onload = function() {
   button.addEventListener('click', function(e) {
     e.preventDefault();
     if (validateEmail() && validatePassword()) {
-      alert('Email: ' + email.value + 'Password: ' + password.value);
+      alert('Email: ' + email.value + '\nPassword: ' + password.value);
     } else if (!validateEmail() && validatePassword()){
         alert('Email is not valid')
     } else if (validateEmail() && !validatePassword()) {
